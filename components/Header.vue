ea9d3d340cf7624ac8c7e2c1ba3787e3274f32b2
cendyz <141049293+cendyz@users.noreply.github.com>
2025-03-30 08:25:18 +0200

    header tablet

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,18 @@
 <template>
-	<header class="overflow-x-clip pb-[10rem]">
+	<header class="overflow-x-clip pb-[10rem] sm:relative">
 		<NuxtImg
 			src="/illustration-working.svg"
 			alt="guy working on computer"
-			class="block w-[full] scale-[1.35] mt-[2rem] ml-[10rem]" />
-		<div class="pt-[8rem] pb-[5rem] text-center px-[2rem]">
-			<h1 class="text-[4rem] font-w700">More than just shorter links</h1>
-			<p class="mt-[1rem] text-[1.8rem] text-neutral-gray-200">Build your brand's recognition and get detailed insights on how you links are preforming.</p>
-			<button class="capitalize mt-[2.5rem] py-[1.2rem] px-[3.5rem] bg-primary-cyan text-[2rem] font-w700 text-white rounded-full">get started</button>
+			class="block w-[full] scale-[1.35] mt-[2rem] ml-[10rem] sm:absolute sm:top-0 sm:left-1/2 sm:translate-x-[-50%] sm:ml-0" />
+		<div class="pt-[8rem] pb-[5rem] text-center px-[2rem] sm:mt-[25rem]">
+			<h1 class="text-[4rem] font-w700 sm:px-[8rem]">More than just shorter links</h1>
+			<p class="mt-[1rem] text-[1.8rem] text-neutral-gray-200 sm:px-[5rem]">
+				Build your brand's recognition and get detailed insights on how you links are preforming.
+			</p>
+			<button
+				class="capitalize mt-[2.5rem] py-[1.2rem] px-[3.5rem] bg-primary-cyan text-[2rem] font-w700 text-white rounded-full">
+				get started
+			</button>
 		</div>
 	</header>
 </template>
