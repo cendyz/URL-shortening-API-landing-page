6d789aa91eb8a529060521c9707aac1dbef49660
cendyz <141049293+cendyz@users.noreply.github.com>
2025-03-31 15:29:30 +0200

    design update

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,9 @@
 <template>
-	<header class="pb-[10rem] sm:relative md:mb-[6rem] md:py-[4rem] lg:container lg:py-[7rem] xl:py-[12rem]">
+	<header class="w-full h-[80rem] sm:relative md:mb-[6rem] md:h-auto md:py-[4rem] lg:container lg:py-[7rem] xl:h-[63rem] xl:flex xl:items-center">
 		<NuxtImg
 			src="/illustration-working.svg"
 			alt="guy working on computer"
-			class="block w-[36rem] scale-[1.35] mt-[2rem] ml-[10rem] sm:absolute sm:top-0 sm:left-1/2 sm:translate-x-[-50%] sm:ml-0 md:translate-x-0 md:scale-[1.2] md:top-[3rem] md:left-[48rem] lg:scale-[1.5] lg:left-[73rem] lg:top-[7rem] xl:top-[13rem] xl:scale-[2] xl:left-[95rem] xl:max-w-[40rem]" />
+			class="block w-[36rem] h-[25rem] scale-[1.35] mt-[2rem] ml-[10rem] sm:absolute sm:top-0 sm:left-1/2 sm:translate-x-[-50%] sm:ml-0 md:translate-x-0 md:scale-[1.2] md:top-[3rem] md:left-[48rem] lg:scale-[1.5] lg:left-[73rem] lg:top-[7rem] xl:top-[13rem] xl:scale-[2] xl:left-[95rem] xl:max-w-[40rem]" />
 		<div
 			class="pt-[8rem] pb-[5rem] text-center px-[2rem] sm:mt-[25rem] sm:w-[40rem] sm:mx-auto md:mt-0 md:pt-0 md:w-[40rem] md:mx-0 md:order-[-1] md:text-left lg:w-[70rem]">
 			<h1 class="text-[4rem] font-w700 lg:text-[6.5rem] lg:leading-[1.2] xl:text-[8rem]">
